015e3b43c68c544e6e3b7056c09c3fcf0d0fc25d
Aashutosh Malviya <aashutoshmalviya000@gmail.com>
2025-07-07 01:25:39 +0530

    Update sms.py

--- sms.py
+++ sms.py
@@ -2,5 +2,5 @@ from flask import Flask
 a=Flask(__name__)
 @a.route('/sms1')
 def lw():
-    return "hwllo "
+    return "Hellow , this is first project of my Devoops jouney using git,github,jenkins,docker,python fully automated.
 a.run(host="0.0.0.0")
